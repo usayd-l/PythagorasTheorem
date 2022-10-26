96f219578960a431138f61350811104b6f04811b
usaydlakhani <usayd.lakhani@gmail.com>
2022-10-26 14:35:56 +0300

    Added rounding to two decimal places functionality

--- pythagoras.py
+++ pythagoras.py
@@ -4,5 +4,6 @@ a = float(input("Give side a: "))
 b = float(input("Give side b: "))
 
 c = math.sqrt(a ** 2 + b ** 2)
+format_c = "{:.2f}".format(c)
 
-print(f"The length of the hypotenuse c is {c}")
+print(f"The length of the hypotenuse c is {format_c}")
